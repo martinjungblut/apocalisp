f6bf0844f50f09edbcb2a3add827e89ae2de11c3
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2019-06-27 04:35:30 -0300

    More progress towards finishing step 1.

--- src/manalispcore/reader.go
+++ src/manalispcore/reader.go
@@ -2,6 +2,7 @@ package manalispcore
 
 import (
 	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+	"strconv"
 )
 
 type reader struct {
@@ -39,10 +40,52 @@ func tokenize(sexpr string) []string {
 }
 
 func readStr(sexpr string) {
-	tokens := tokenize(sexpr)
 	reader := reader{
 		position: 0,
-		tokens:   tokens,
+		tokens:   tokenize(sexpr),
 	}
+
 	readForm(reader)
+}
+
+type MalType struct {
+	_integer *int64
+	_symbol  *string
+	_list    []MalType
+}
+
+func readForm(r Reader) MalType {
+	token := r.peek()
+
+	if token == "(" {
+		return readList(r)
+	} else {
+		return readAtom(r)
+	}
+}
+
+func readList(r Reader) MalType {
+	list := MalType{_list: make([]MalType, 0)}
+
+	for {
+		token := r.next()
+		if token == ")" {
+			break
+		} else {
+			list._list = append(list._list, readForm(r))
+		}
+	}
+
+	return list
+}
+
+func readAtom(r Reader) MalType {
+	token := r.next()
+
+	i, err := strconv(token, 10, 64)
+	if err == nil {
+		return MalType{_integer: &i}
+	}
+
+	return MalType{_symbol: &token}
 }
